a886309013ca9e2384ed7ea5fcece56c55f8173a
denrus2003 <denverrus2003@gmail.com>
2024-01-08 22:20:05 +0300

    Доработка интерфейса и функционала + ЛК
    Форма для создания заявки, Оформление в соответствии с цветовой схемой, Макет ЛК

--- main_functions.js
+++ main_functions.js
@@ -150,7 +150,98 @@ function fetchRoutes() {
     // Отправка запроса
     request.send();
 }
+$(document).ready(function() {
+    $(".btn-primary").click(function() {
+        $("#orderModal").modal('show');
+    });
+});
+
+
+// eslint-disable-next-line max-len
+function calculatePrice(guideServiceCost, hoursNumber, isThisDayOff, startTime, numberOfVisitors, isPensioner, isTransfer) {
+    // Множитель для выходных и праздничных дней
+    var dayOffMultiplier = isThisDayOff ? 1.5 : 1;
+
+    // Надбавка за раннее время
+    var isItMorning = (startTime >= 9 && startTime < 12) ? 400 : 0;
+
+    // Надбавка за вечернее время
+    var isItEvening = (startTime >= 20 && startTime < 23) ? 1000 : 0;
+
+    // Надбавка за количество посетителей
+    var visitorsFee;
+    if (numberOfVisitors <= 5) {
+        visitorsFee = 0;
+    } else if (numberOfVisitors <= 10) {
+        visitorsFee = 1000;
+    } else if (numberOfVisitors <= 20) {
+        visitorsFee = 1500;
+    }
+
+    // Расчет стоимости
+    // eslint-disable-next-line max-len
+    var price = guideServiceCost * hoursNumber * dayOffMultiplier + isItMorning + isItEvening + visitorsFee;
+
+    // Применение скидки для пенсионеров
+    if (isPensioner) {
+        price *= 0.75;
+    }
+
+    // Учет стоимости трансфера
+    if (isTransfer) {
+        price *= isThisDayOff ? 1.25 : 1.3;
+    }
+
+    return price;
+}
+
+
+$(document).ready(function() {
+    $(".btn-primary").click(function() {
+        $("#orderModal").modal('show');
+    });
+});
+
+// eslint-disable-next-line max-len
+document.getElementById('orderModal').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Сбор данных формы
+    var data = {
+        routeName: document.getElementById('routeName').value,
+        guideName: document.getElementById('guideName').value,
+        excursionDate: document.getElementById('excursionDate').value,
+        startTime: document.getElementById('startTime').value,
+        duration: document.getElementById('duration').value,
+        groupSize: document.getElementById('groupSize').value,
+        isPensioner: document.getElementById('isPensioner').checked,
+        isTransfer: document.getElementById('isTransfer').checked,
+    };
+    // Расчет стоимости
+    // eslint-disable-next-line max-len
+    data.totalCost = calculatePrice(/* здесь должны быть переданы параметры для расчета стоимости */);
+
+    // Отправка данных на сервер
+    fetch('http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/orders', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer d71c466e-977a-4208-8a10-75063f17000f'
+        },
+        body: JSON.stringify(data)
+    }).then(function(response) {
+        if (response.ok) {
+            // Заявка успешно отправлена
+            alert('Заявка успешно отправлена!');
+        } else {
+            // Произошла ошибка
+            alert('Произошла ошибка при отправке заявки.');
+        }
+    });
+});
+
 
 // Вызов функций при загрузке страницы
-window.onload = showGuides;
-window.onload = fetchRoutes;
+window.onload = function() {
+    fetchRoutes();
+};
